277743aa829ecf8258cb2fefad289191d4821ae1
NateOwens <nathanieltylerowens@gmail.com>
2020-10-13 19:14:09 -0500

    finished seven and started eight i guess?

--- models/animals.py
+++ models/animals.py
@@ -3,11 +3,11 @@ class Animal():
     # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, name, species, status, locationId, customerId):
+    def __init__(self, id, name, species, status, locationId):
+        self.id = id
         self.name = name
         self.species = species
         self.status = status
         self.locationId = locationId
-        self.customerId = customerId
     def __repr__(self):
         return f"{self.name} is a {self.species}"
