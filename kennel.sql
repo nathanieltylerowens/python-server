079c6feeb3635f303a7c713ee49869ec39064a24
NateOwens <nathanieltylerowens@gmail.com>
2020-10-15 21:00:33 -0500

    finished 9 those chapters suck and aren't helpful about troubleshooting

--- kennel.sql
+++ kennel.sql
@@ -1,34 +1,16 @@
-CREATE TABLE `Location` (
-	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`name`	TEXT NOT NULL,
-	`address`	TEXT NOT NULL
-);
-
-CREATE TABLE `Customer` (
-    `id`    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    `name`    TEXT NOT NULL,
-    `address`    TEXT NOT NULL,
-    `email`    TEXT NOT NULL,
-    `password`    TEXT NOT NULL
-);
-
-CREATE TABLE `Animal` (
-	`id`  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`name`  TEXT NOT NULL,
-	`status` TEXT NOT NULL,
-	`breed` TEXT NOT NULL,
-	`customer_id` INTEGER NOT NULL,
-	`location_id` INTEGER,
-	FOREIGN KEY(`customer_id`) REFERENCES `Customer`(`id`),
-	FOREIGN KEY(`location_id`) REFERENCES `Location`(`id`)
-);
+INSERT INTO `Location` VALUES (null, 'Nashville North', "64 Washington Heights");
+INSERT INTO `Location` VALUES (null, 'Nashville South', "101 Penn Ave");
 
 
-CREATE TABLE `Employee` (
-	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`name`	TEXT NOT NULL,
-	`address`	TEXT NOT NULL,
-	`location_id` INTEGER NOT NULL,
-	FOREIGN KEY(`location_id`) REFERENCES `Location`(`id`)
+INSERT INTO `Employee` VALUES (null, "Madi Peper", "35498 Madison Ave", 1);
+INSERT INTO `Employee` VALUES (null, "Kristen Norris", "100 Main St", 1);
+INSERT INTO `Employee` VALUES (null, "Meg Ducharme", "404 Unknown Ct", 2);
+INSERT INTO `Employee` VALUES (null, "Hannah Hall", "204 Empty Ave", 1);
+INSERT INTO `Employee` VALUES (null, "Leah Hoefling", "200 Success Way", 2);
 
-);
+
+INSERT INTO `Animal` VALUES (null, "Snickers", "Recreation", "Dalmation", 1);
+INSERT INTO `Animal` VALUES (null, "Jax", "Treatment", "Beagle", 1);
+INSERT INTO `Animal` VALUES (null, "Falafel", "Treatment", "Siamese", 2);
+INSERT INTO `Animal` VALUES (null, "Doodles", "Kennel", "Poodle", 1);
+INSERT INTO `Animal` VALUES (null, "Daps", "Kennel", "Boxer", 2);
